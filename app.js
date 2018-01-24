5c03d4597ffdbc4d0c1672d22394f82ec19db158
Dan Rosen <drosen84@gmail.com>
2018-01-24 17:55:02 -0600

    moved token redirect into event handler

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const hash = window.location.hash
   .substring(1)
   .split('&')
   .reduce(function (initial, item) {
+    console.log('hash set');
     if (item) {
       var parts = item.split('=');
       initial[parts[0]] = decodeURIComponent(parts[1]);
@@ -12,9 +13,10 @@ const hash = window.location.hash
     return initial;
   }, {});
 window.location.hash = '';
-
+ 
 // Set token
 let _token = hash.access_token;
+console.log(`token is ${_token}`);
 
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 
@@ -23,21 +25,39 @@ const clientId = '5f795f8bb8c14d94bafa6dcd2ed3038b';
 const redirectUri = 'http://localhost:8888/index';
 
 // If there is no token, redirect to Spotify authorization
-if (!_token) {
-  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
-}
+// if (!_token) {
+//   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
+// }
 
 // Make a call using the token
-$.ajax({
-  url: 'https://api.spotify.com/v1/me/top/artists',
-  type: 'GET',
-  beforeSend: function(xhr){
-    xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
-  success: function(data) { 
-    // Do something with the returned data
-    data.items.map(function(artist) {
-      let item = $('<li>' + artist.name + '</li>');
-      item.appendTo($('#top-artists'));
-    });
-  }
-});
+// $.ajax({
+//   url: 'https://api.spotify.com/v1/me/top/artists',
+//   type: 'GET',
+//   beforeSend: function(xhr){
+//     xhr.setRequestHeader('Authorization', 'Bearer ' + _token );},
+//   success: function(data) { 
+//     // Do something with the returned data
+//     data.items.map(function(artist) {
+//       let item = $('<li>' + artist.name + '</li>');
+//       item.appendTo($('#top-artists'));
+//     });
+//   }
+// });
+
+function watchSubmit() {
+  $('.js-artist-search').submit(event => {
+    console.log('submit button clicked');
+    event.preventDefault();
+    if (!_token) {
+      window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
+    } else {
+      const queryBand = $(event.currentTarget).find('.js-query');
+      const query = queryBand.val();
+      console.log(`You searched for ${query}`);  
+      queryBand.val('');
+    // getDataFromApi(query, displayYTSearchData);
+    }
+  });
+}
+
+$(watchSubmit);
